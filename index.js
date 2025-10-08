45854048472c9fa8d9de4575187f84b36b3f3487
Kalyan Shilpa Rani <kalyanshilpa2003@gmail.com>
2025-10-08 12:55:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,89 +1,4 @@
-// button=document.querySelector("#start");
-// button.addEventListener("click",()=>{
-//         oncllick();
-// });
-// function oncllick(){
-// arr=[];
-// for(var i=1;i<=100;i++){
-//     arr.push(i);
-// }
-// // Fisher yates Shuffle algorithm.
-// function randomize (arr)
-// {
-//     for (let i = arr.length - 1; i >= 0; i--)
-//     {    
-//         let j = Math.floor(Math.random() * (i + 1));
-//         [arr[i], arr[j]] = [arr[j], arr[i]];
-//     }
-// }
-// var hitval;
-// function getNewHit(){
-//     hitval=Math.floor(Math.random()*100);
-//     document.querySelector("#hit").textContent=hitval;
-// }
-// function makeBubble(){
-//     randomize(arr);
-//     clutter="";
-//     for(var i=1;i<=144;i++){
-//         var rnumber;
-//         if(i<=99){
-//             rnumber=arr[i];
-//             clutter+=`<div class="bubble">${rnumber}</div>`;
-//         }
-//         // if(i==100){
-//         //     rnumber=Math.floor(Math.random()*100);
-//         //     clutter+=`<div class="bubble">${rnumber}</div>`;
-//         // }
-//         else{
-//             rnumber=Math.floor(Math.random()*100);
-//             clutter+=`<div class="bubble">${rnumber}</div>`;
-//         }
-//     }
-//     document.querySelector("#pbtm").innerHTML=clutter;
-    
-// }
-// var timer=60;
-// function runtimer(){
-//     setInterval(function(){
-//         if(timer>0){
-//             timer--;
-//         document.querySelector("#timer").textContent=timer;
-//         }
-//         else{
-//             document.querySelector("#pbtm").innerHTML=`<h1>Game Over.Your Score:${score}</h1>`;
-//             clearInterval(timer);
-//         }
-//     },1000)
-// }
-// var score=0;
-// function updateScore(){
-//     document.querySelector("#pbtm").addEventListener('click',function(event){
-//         // console.log(event.target.textContent);
-//        // console.log(hitval);
-//         if(Number(event.target.textContent)===hitval){
-//             score+=10;
-//         }
-//         document.querySelector("#score").textContent=score;
-//         getNewHit();
-//         makeBubble();
-//     })
-// }
-// getNewHit();
-// makeBubble();
-// runtimer();
-// updateScore();
-// }
 
-
-// const button = document.querySelector("#start");
-
-// button.addEventListener("click", () => {
-//     button.disabled = true; // Disable the button
-//     oncllick(); // Call the main function
-// });
-
-// function oncllick() {
-    // button.disabled=true;
     arr = [];
 
     for (var i = 1; i <= 100; i++) {
@@ -157,4 +72,5 @@
 
     // Re-enable the button after everything is done
 // }
+
 
